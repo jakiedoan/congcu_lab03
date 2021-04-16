5244533580ba13c848c7104826cc510c9b21cbcb
jakiedoan <jake.jalance@gmail.com>
2021-04-16 09:18:41 +0700

    Them 1 class

---
+++ Project_Lab03/Project_Lab03/App_Start/Lab3Git.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_Lab03.App_Start
+{
+    public class Lab3Git
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
